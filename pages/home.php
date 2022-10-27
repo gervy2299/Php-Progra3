5fc145a4e1f15040bd83061fd681728e173ab2f0
Gervy Salinas <play_main_1001@outlook.com>
2022-10-27 02:26:44 -0500

    quitar

--- pages/home.php
+++ pages/home.php
@@ -2,17 +2,6 @@
 
 include_once './conexion.php';
 
-$nm = "on";
-    // array_key_exists('logina', $_POST){} cambio();
-
-    function cambio($val){
-        if($val = "on"){
-            $val = "off";
-        }elseif($val = "off"){
-            $val = "on";
-        }
-    }
-
 ?>
 
 <!DOCTYPE html>
@@ -26,15 +15,10 @@ $nm = "on";
 </head>
 
 <body>
-    <input type="button" value="Esto es un boton" name="cambio" onclick='<?php cambio($nm)?>'>
-    <?php
-    
-    if ($nm = "on") {
-        include_once './almacen.php';
-    } elseif ($nm = "off") {
-        include_once './proveedores.php';
-    }
-    ?>
+
+    <input type="button" value="almacen">
+    <input type="button" value="proveedores">
+
 </body>
 
 </html>
