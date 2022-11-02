7c7d7c31626ccad3f4d8b8545bc625a1e041b5eb
gervy salinas <play_main_1001@outlook.com>
2022-11-02 15:02:33 -0500

    mostrando modulos vacios en el nav

--- pages/conexion.php
+++ pages/conexion.php
@@ -103,29 +103,29 @@
            href="#"
            class="list-group-item list-group-item-action py-2 ripple"
            ><i class="fas fa-globe fa-fw me-3"></i
-          ><span>International</span></a
+          ><span></span></a
           >
         <a
            href="#"
            class="list-group-item list-group-item-action py-2 ripple"
            ><i class="fas fa-building fa-fw me-3"></i
-          ><span>Partners</span></a
+          ><span></span></a
           >
         <a
            href="#"
            class="list-group-item list-group-item-action py-2 ripple"
            ><i class="fas fa-calendar fa-fw me-3"></i
-          ><span>Calendar</span></a
+          ><span></span></a
           >
         <a
            href="#"
            class="list-group-item list-group-item-action py-2 ripple"
-           ><i class="fas fa-users fa-fw me-3"></i><span>Users</span></a
+           ><i class="fas fa-users fa-fw me-3"></i><span></span></a
           >
         <a
            href="#"
            class="list-group-item list-group-item-action py-2 ripple"
-           ><i class="fas fa-money-bill fa-fw me-3"></i><span>Sales</span></a
+           ><i class="fas fa-money-bill fa-fw me-3"></i><span></span></a
           >
       </div>
     </div>
